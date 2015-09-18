ac7861d64277cfd6242ed4739840522f40cbe710
Edvinas <edvnaz@gmail.com>
2015-09-18 15:16:58 +0300

    update

--- app/src/main/java/com/example/ed/twittertimelinesearch/MainActivity.java
+++ app/src/main/java/com/example/ed/twittertimelinesearch/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
