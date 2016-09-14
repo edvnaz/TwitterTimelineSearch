696f7f8b0bb20e605490051add10cd69f33f23b2
Ed <edvnaz@gmail.com>
2016-09-15 01:26:49 +0300

    added img

--- app/src/main/java/com/example/ed/twittertimelinesearch/MainActivity.java
+++ app/src/main/java/com/example/ed/twittertimelinesearch/MainActivity.java
@@ -2,14 +2,10 @@ package com.example.ed.twittertimelinesearch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
